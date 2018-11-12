095051c5acd0f19c04c2c64f334e32d158e52bb2
Han Seob Kim <khseob0715@gmail.com>
2018-11-13 00:30:16 +0900

    animation

--- 01 Script/MovingObject.cs
+++ 01 Script/MovingObject.cs
@@ -19,55 +19,73 @@ public class MovingObject : MonoBehaviour
 
     private bool canMove = true;  // 코루틴이 계속 호출 되는 것 방지 
 
+    private Animator animator;
+
     // Use this for initialization
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     IEnumerator MoveCoroutine()
     {
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
-            applyRunSpeed = runSpeed;
-            applyRunFlag = true;
-        }
-        else
-        {
-            applyRunSpeed = 0;
-            applyRunFlag = false;
-        }
-
-        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-        // z 축은 변하지 않는 값
-
-        while (currentWalkCount < walkCount)
-        {
-            if (vector.x != 0)
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
-
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
             }
-            else if (vector.y != 0)
+            else
             {
-                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
-                // Translate 함수는 현재 있는 값에서 주어진 값 만큼 더해주는 것
+                applyRunSpeed = 0;
+                applyRunFlag = false;
             }
-            if (applyRunFlag)
+
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+            // z 축은 변하지 않는 값
+
+            if(vector.x != 0)
             {
+                vector.y = 0;
+            }
+
+            if(vector.y != 0)
+            {
+                vector.x = 0;
+            }
+            animator.SetFloat("DirX", vector.x); // vector.x 의 값을 DirX로 전달.
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while (currentWalkCount < walkCount)
+            {
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                    // Translate 함수는 현재 있는 값에서 주어진 값 만큼 더해주는 것
+                }
+                if (applyRunFlag)
+                {
+                    currentWalkCount++;
+                }
                 currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);
             }
-            currentWalkCount++;
-            yield return new WaitForSeconds(0.01f);
+            currentWalkCount = 0;
+
         }
-        currentWalkCount = 0;
 
+        animator.SetBool("Walking", false);
 
-
-        // 1초 동안 대기. 
 
         canMove = true;
+
     }
 
     // Update is called once per frame
