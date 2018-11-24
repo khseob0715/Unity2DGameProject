fd134ed6f2d9af60affa7587cd148b7215cc2c90
Han Seob Kim <khseob0715@gmail.com>
2018-11-24 19:29:28 +0900

    test

---
+++ Assets/01 Script/BGMManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour {
+
+    public static BGMManager instance;
+
+    public AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private WaitForSeconds waitTime = new WaitForSeconds(0.01f);
+
+    private void Awake()
+    {
+        // start 보다 먼저 실행 됨. 
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+
+
+	}
+
+    public void Play(int _playMusicTrack)
+    {
+        audioSource.volume = 1f;
+        audioSource.clip = clips[_playMusicTrack];
+        audioSource.Play();
+    }
+
+    public void Stop()
+    {
+        audioSource.Stop();
+    }
+
+
+    public void Pause()
+    {
+        audioSource.Pause();
+    }
+
+
+    public void UnPause()
+    {
+        audioSource.UnPause();
+    }
+
+
+    public void SetVolumn(float _volumn)
+    {
+        audioSource.volume = _volumn;
+    }
+
+
+    public void FadeOutMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutMusicCoroutine());
+    }
+
+    public void FadeInMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInMusicCoroutine());
+    }
+	
+    IEnumerator FadeOutMusicCoroutine()
+    {
+        for (float i = 1.0f; i >= 0f; i -= 0.01f)
+        {
+            audioSource.volume = i;
+            yield return waitTime; // 성능상 이득. 
+        }
+    }
+
+
+    IEnumerator FadeInMusicCoroutine()
+    {
+        for (float i = 0.0f; i <= 1f; i += 0.01f)
+        {
+            audioSource.volume = i;
+            yield return waitTime; // 성능상 이득. 
+        }
+    }
+}
+
---
+++ Assets/01 Script/Test1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+    BGMManager BGM;
+
+    public int playMusicTrack;
+
+	// Use this for initialization
+	void Start () {
+        BGM = FindObjectOfType<BGMManager>();
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        BGM.Play(playMusicTrack);
+        this.gameObject.SetActive(false);
+    }
+}
